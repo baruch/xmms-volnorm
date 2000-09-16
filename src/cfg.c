fd9f526ea38444f2df8ae5e9dc2a769fdf29c2ff
Baruch Even <baruch@ev-en.org>
2000-09-16 06:02:58 +0000

    Various small changes.

--- src/cfg.c
+++ src/cfg.c
@@ -7,7 +7,7 @@
 
 #define DEFAULT_LEVEL 0.25
 #define DEFAULT_SILENCE 0.01
-#define DEFAULT_MAX_MULT 15
+#define DEFAULT_MAX_MULT 15.0
 
 double normalize_level = DEFAULT_LEVEL;
 double silence_level = DEFAULT_SILENCE;
@@ -17,10 +17,13 @@ void read_config(void)
 {
 	ConfigFile * config = xmms_cfg_open_default_file();
 
+	/* Load the values, if it fails, set default value */
 	if (!xmms_cfg_read_double(config, "normvol", "level", &normalize_level))
 		normalize_level = DEFAULT_LEVEL;
+	
 	if (!xmms_cfg_read_double(config, "normvol", "silence", &silence_level))
 		normalize_level = DEFAULT_SILENCE;
+	
 	if (!xmms_cfg_read_double(config, "normvol", "maxmult", &max_mult))
 		normalize_level = DEFAULT_MAX_MULT;
 
